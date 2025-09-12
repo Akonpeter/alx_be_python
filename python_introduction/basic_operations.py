9a198e194d3915b16c0d78e45056d67d2ae2c5f5
Akon peter <prizylink@gmail.com>
2025-09-12 02:34:38 -0700

    basic python introduction

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,17 @@
+# addition
+number1 = 10
+number2 = 5 
+x = number1 + number2
+print(f"Addition of {number1} and {number2} is {x}")
+ 
+#  subtraction
+number1 = 10
+number2 = 5 
+x = number1 - number2
+print(f"subtraction of {number1} and {number2} is {x}")
+
+# multiplication
+number1 = 10
+number2 = 5 
+x = number1 * number2
+print(f"multiplication of {number1} and {number2} is {x}")
