84026b7f9e52d65cda98bc57e3647df89ca992c3
Akon peter <prizylink@gmail.com>
2025-09-18 11:28:20 -0700

    Weather Recommendation Program

---
+++ Checking Eligibility.py
@@ -0,0 +1,9 @@
+# Checking Eligibility with if and else
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote yet.")
+    
---
+++ control-flow/weather_advice.py
@@ -0,0 +1,15 @@
+# Weather advice
+
+# Prompt the user for the weather
+
+weather = input("What's the weather like today? (sunny/rainy/cold):").lower()
+
+# Provide clothing recommendation
+if weather == "sunny":
+    print("Wear a t-shirt and sunglasses.")
+elif weather == "rainy":
+    print("Don't forget your umbrella and raincoat.")
+elif weather == "cold":
+    print("Make sure to wear a warm coat and a scarf.")
+else:
+    print("Sorry, I don't have recommendations for this weather.")
