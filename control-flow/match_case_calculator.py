2bb5481708c163f11ae85f73b56f370b692be814
Akon peter <prizylink@gmail.com>
2025-09-19 16:00:41 -0700

    Multiplication Table Generator

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -5,7 +5,7 @@
 num1 = float (input("Enter the first number:") )
 num2 = float (input("Enter the second number:") )
 
-operation = input("Choose an operation ( +, -, *, / ): ")
+operation = input("Choose an operation ( +, -, *, /,  ) ")
 
 # Match Case for operations
 
@@ -33,12 +33,5 @@ match operation:
     case "/":
          result = num1 / num2
          print("The result  is: (result).")
-         if num2 != 0:
-    
-          print("Error: Division by zero is not allowed") 
-             
-              
-
-
-
-
+         
+       
---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,7 @@
+ # Multiplication
+
+number = int(input("Enter a number to see its multiplication: "))
+
+for i in range( 1, 11 ):
+    product = number * i
+    print(f"{number} * {i} = {product}")
