3f16f1356333ed71eb048a76fe7c7c1f7c14569e
Akon peter <prizylink@gmail.com>
2025-09-21 05:07:21 -0700

    Simple Calculator with Match Case

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,42 +1,7 @@
-# # Calculator
 
-# # Prompt the user for operation 
-
-# num1 = float (input("Enter the first number:") )
-# num2 = float (input("Enter the second number:") )
-
-# operation = input("Choose an operation ( +, -, *, /,: )")
-
-# # Match Case for operations
-
-# match operation:
-
-#     # Addition
-
-#     case "+.":
-#         result = num1 + num2
-#         print(f"The result  is: {result}")
-
-# # Subtraction
-
-#     case "-":
-#         result = num1 - num2
-#         print(f"The result  is: {result}")
-
-# # Multiplication
-
-#     case  "*": 
-#         result = num1 * num2
-#         print(f"The result  is: {result}")
-
-# # Divide 
-#     case "/":
-#          result = num1 / num2
-#          print("The result  is: (result).")
-
-# calculator.py
 
 # Prompt the user for numbers and operation
+
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 operation = input("Choose an operation (+, -, *, /): ")
